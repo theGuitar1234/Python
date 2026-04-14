1a4045389284bf8534dacf4655b8d1b7e02433c6
The Guitar <turanlizad776@gmail.com>
2026-04-14 16:06:55 +0400

    513236fa-8248-4781-8cca-c1f692fd678d

--- matplotlib/plots/Stem_Plot.py
+++ matplotlib/plots/Stem_Plot.py
@@ -1,1 +1,13 @@
-import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0.1, 2 * np.pi, 41)
+y = np.exp(np.sin(x))
+
+markerline, stemlines, baseline = plt.stem(
+    x, y, linefmt ='grey', markerfmt ='D',
+    bottom = 1.1)
+markerline.set_markerfacecolor('none')
+# plt.stem(x, y)
+plt.savefig("img/stm_plt.png")
+plt.show()
