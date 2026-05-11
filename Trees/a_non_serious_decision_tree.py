03bfd0d14eb9a46ad891ee1e41fd2f3ff25fcc1a
The Guitar <turanlizad776@gmail.com>
2026-05-11 17:41:37 +0400

    770167fc-bf03-4337-a21e-c435da930c79

--- Trees/a_non_serious_decision_tree.py
+++ Trees/a_non_serious_decision_tree.py
@@ -1,3 +1,6 @@
+from dataclasses import dataclass
+from enum import Enum
+
 import numpy as np
 import sys
 
@@ -12,11 +15,28 @@ class Node:
 
 
 class ANonSeriousDecisionTree:
-    def __init__(self, max_depth=3, categorical=False, adjacent=False):
+
+    @dataclass
+    class InformationGain(Enum):
+        GINI = 1
+        ENTROPY = 2
+
+    def __init__(
+        self,
+        minimum_population_size=2,
+        minimum_split_size=1,
+        max_depth=3,
+        categorical=False,
+        adjacent=False,
+        information_gain=None,
+    ):
         self.root = None
         self.max_depth = max_depth
         self.categorical = categorical
         self.adjacent = adjacent
+        self.minimum_population_size = minimum_population_size
+        self.minimum_split_size = minimum_split_size
+        self.information_gain = information_gain
 
     def fit(self, X, y):
         if X.shape[0] != y.shape[0]:
@@ -28,7 +48,11 @@ class ANonSeriousDecisionTree:
     def _build_tree(self, X, y, depth):
         node = Node()
 
-        if depth >= self.max_depth or len(set(y)) == 1:
+        if (
+            depth >= self.max_depth
+            or len(set(y)) == 1
+            or len(y) <= self.minimum_population_size
+        ):
             node.value = self._majority_class(y)
             return node
         feature, threshold = self._choose_split(X, y)
@@ -45,6 +69,14 @@ class ANonSeriousDecisionTree:
         else:
             left_mask = X[:, feature] > threshold
             right_mask = X[:, feature] <= threshold
+
+        if (
+            len(X[left_mask]) < self.minimum_split_size
+            or len(X[right_mask]) < self.minimum_split_size
+        ):
+            node.value = self._majority_class(y)
+            return node
+
         node.left = self._build_tree(X[left_mask], y[left_mask], depth + 1)
         node.right = self._build_tree(X[right_mask], y[right_mask], depth + 1)
 
@@ -66,7 +98,6 @@ class ANonSeriousDecisionTree:
                 if self.adjacent:
                     sorted_y = y[order]
                     mask = sorted_y[1:] != sorted_y[:-1]
-                    # mask = self.adjacent_classes(sorted_y)
                     categories = (categories[1:][mask] + categories[:-1][mask]) / 2
                 else:
                     categories = (categories[1:] + categories[:-1]) / 2
@@ -82,22 +113,28 @@ class ANonSeriousDecisionTree:
                 left_group = y[left_mask]
                 right_group = y[right_mask]
 
-                __gini_left = self.gini(left_group)
-                __gini_right = self.gini(right_group)
+                match self.information_gain:
+                    case self.InformationGain.GINI:
+                        __left = self.gini(left_group)
+                        __right = self.gini(right_group)
+                    case self.InformationGain.ENTROPY:
+                        __left = self.entropy(left_group)
+                        __right = self.entropy(right_group)
+                    case _:
+                        raise ValueError(
+                            f"Unsupported Information Gain, supported values are {list(self.InfomrationGain)}"
+                        )
 
-                weighted_gini = self.gini_weighted(
-                    __gini_left, __gini_right, len(left_group), len(right_group)
+                weighted_gain = self.weighted_gain(
+                    __left, __right, len(left_group), len(right_group)
                 )
 
-                if weighted_gini < best_gini:
-                    best_gini = weighted_gini
+                if weighted_gain < best_gini:
+                    best_gini = weighted_gain
                     best_feature = feature
                     best_threshold = category
-        return best_feature, best_threshold
 
-    # def adjacent_classes(self, sorted_y):
-    #     mask = sorted_y[1:] != sorted_y[:-1]
-    #     return mask
+        return best_feature, best_threshold
 
     def gini(self, y):
         if len(y) == 0:
@@ -106,11 +143,16 @@ class ANonSeriousDecisionTree:
         proportions = counts / len(y)
         return 1 - np.sum(proportions**2)
 
-    def gini_weighted(self, gini_left, gini_right, left_size, right_size):
+    def entropy(self, y):
+        if len(y) == 0:
+            return 0
+        _, counts = np.unique(y, return_counts=True)
+        proportions = counts / len(y)
+        return -np.sum(proportions * np.log2(proportions))
+
+    def weighted_gain(self, left, right, left_size, right_size):
         total_size = left_size + right_size
-        return (left_size / total_size) * gini_left + (
-            right_size / total_size
-        ) * gini_right
+        return (left_size / total_size) * left + (right_size / total_size) * right
 
     def predict_one(self, x, node=None):
         if node is None:
@@ -139,7 +181,12 @@ class ANonSeriousDecisionTree:
 
 
 if __name__ == "__main__":
-    tree = ANonSeriousDecisionTree(categorical=False, adjacent=True)
+    tree = ANonSeriousDecisionTree(
+        2,
+        categorical=False,
+        adjacent=True,
+        information_gain=ANonSeriousDecisionTree.InformationGain.ENTROPY,
+    )
     # X = [
     #     [2, 1],
     #     [2, 0],
