356e1c4ac479ae990afd93cec0bbd6b803047fe0
The Guitar <turanlizad776@gmail.com>
2026-05-11 12:45:57 +0400

    fb2eab62-b2d5-46c2-a485-0c6eed2dd371

--- Trees/a_non_serious_decision_tree.py
+++ Trees/a_non_serious_decision_tree.py
@@ -1,4 +1,5 @@
 import numpy as np
+import sys
 
 
 class Node:
@@ -11,13 +12,17 @@ class Node:
 
 
 class ANonSeriousDecisionTree:
-    def __init__(self, max_depth=3):
-        self.max_depth = max_depth
+    def __init__(self, max_depth=3, categorical=False, adjacent=False):
         self.root = None
+        self.max_depth = max_depth
+        self.categorical = categorical
+        self.adjacent = adjacent
 
     def fit(self, X, y):
-        X = np.array(X)
-        y = np.array(y)
+        if X.shape[0] != y.shape[0]:
+            raise ValueError(
+                f"X and y must match in shapes : {X.shape[0]} != {y.shape[0]}"
+            )
         self.root = self._build_tree(X, y, depth=0)
 
     def _build_tree(self, X, y, depth):
@@ -26,19 +31,20 @@ class ANonSeriousDecisionTree:
         if depth >= self.max_depth or len(set(y)) == 1:
             node.value = self._majority_class(y)
             return node
-
         feature, threshold = self._choose_split(X, y)
 
         if feature is None:
             node.value = self._majority_class(y)
             return node
-
         node.feature = feature
         node.threshold = threshold
 
-        left_mask = X[:, feature] == threshold
-        right_mask = X[:, feature] != threshold
-
+        if self.categorical:
+            left_mask = X[:, feature] == threshold
+            right_mask = X[:, feature] != threshold
+        else:
+            left_mask = X[:, feature] > threshold
+            right_mask = X[:, feature] <= threshold
         node.left = self._build_tree(X[left_mask], y[left_mask], depth + 1)
         node.right = self._build_tree(X[right_mask], y[right_mask], depth + 1)
 
@@ -51,16 +57,28 @@ class ANonSeriousDecisionTree:
 
         _, features = X.shape
         for feature in range(features):
-            values = X[:, feature]
-            categories = np.unique(values)
+            values = np.sort(X[:, feature])
+            order = np.argsort(values)
+            sorted_values = values[order]
+            categories = np.unique(sorted_values)
 
+            if not self.categorical:
+                if self.adjacent:
+                    sorted_y = y[order]
+                    mask = sorted_y[1:] != sorted_y[:-1]
+                    # mask = self.adjacent_classes(sorted_y)
+                    categories = (categories[1:][mask] + categories[:-1][mask]) / 2
+                else:
+                    categories = (categories[1:] + categories[:-1]) / 2
             for category in categories:
-                left_mask = (values == category)
-                right_mask = (values != category)
-
+                if self.categorical:
+                    left_mask = values == category
+                    right_mask = values != category
+                else:
+                    left_mask = values > category
+                    right_mask = values <= category
                 if np.sum(left_mask) == 0 or np.sum(right_mask) == 0:
                     continue
-
                 left_group = y[left_mask]
                 right_group = y[right_mask]
 
@@ -68,15 +86,18 @@ class ANonSeriousDecisionTree:
                 __gini_right = self.gini(right_group)
 
                 weighted_gini = self.gini_weighted(
-                    __gini_left, __gini_right, len(values)
+                    __gini_left, __gini_right, len(left_group), len(right_group)
                 )
 
                 if weighted_gini < best_gini:
                     best_gini = weighted_gini
                     best_feature = feature
                     best_threshold = category
-
         return best_feature, best_threshold
+
+    # def adjacent_classes(self, sorted_y):
+    #     mask = sorted_y[1:] != sorted_y[:-1]
+    #     return mask
 
     def gini(self, y):
         if len(y) == 0:
@@ -85,10 +106,11 @@ class ANonSeriousDecisionTree:
         proportions = counts / len(y)
         return 1 - np.sum(proportions**2)
 
-    def gini_weighted(self, gini_left, gini_right, total_size):
-        return ((np.size(gini_left) / total_size) * gini_left) + (
-            (np.size(gini_right) / total_size) * gini_right
-        )
+    def gini_weighted(self, gini_left, gini_right, left_size, right_size):
+        total_size = left_size + right_size
+        return (left_size / total_size) * gini_left + (
+            right_size / total_size
+        ) * gini_right
 
     def predict_one(self, x, node=None):
         if node is None:
@@ -97,10 +119,16 @@ class ANonSeriousDecisionTree:
         if node.value is not None:
             return node.value
 
-        if x[node.feature] == node.threshold:
-            return self.predict_one(x, node.left)
+        if self.categorical:
+            if x[node.feature] == node.threshold:
+                return self.predict_one(x, node.left)
+            else:
+                return self.predict_one(x, node.right)
         else:
-            return self.predict_one(x, node.right)
+            if x[node.feature] > node.threshold:
+                return self.predict_one(x, node.left)
+            else:
+                return self.predict_one(x, node.right)
 
     def predict(self, X):
         X = np.array(X)
@@ -111,26 +139,39 @@ class ANonSeriousDecisionTree:
 
 
 if __name__ == "__main__":
-    tree = ANonSeriousDecisionTree()
+    tree = ANonSeriousDecisionTree(categorical=False, adjacent=True)
+    # X = [
+    #     [2, 1],
+    #     [2, 0],
+    #     [2, 0],
+    #     [1, 1],
+    #     [1, 0],
+    #     [0, 1],
+    #     [0, 0],
+    #     [0, 0],
+    # ]
+    # y = [1, 1, 1, 1, 0, 1, 0, 0]
     X = [
-        [2, 1],
-        [2, 0],
-        [2, 0],
-        [1, 1],
-        [1, 0],
-        [0, 1],
-        [0, 0],
-        [0, 0],
+        [60],
+        [70],
+        [75],
+        [85],
+        [90],
+        [95],
+        [100],
+        [110],
+        [120],
+        [125],
     ]
-    y = [1, 1, 1, 1, 0, 1, 0, 0]
+    y = [0, 0, 0, 1, 1, 1, 0, 0, 0, 0]
 
     X = np.asarray(X, dtype=np.int32)
     y = np.asarray(y, dtype=np.int32)
 
     tree.fit(X, y)
     input = [
-        [1, 1],
-        [1, 0],
-        [0, 0],
+        [72],
+        [96],
+        [200],
     ]
     print(tree.predict(input))
